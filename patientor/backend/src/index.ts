c955106e4c2f25fc7f1724842f249fc37ee7443c
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-29 08:36:04 +0800

    9.11 app uses patients router, uses cors

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import diagnosisRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const PORT = 3001;
@@ -11,6 +14,7 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
