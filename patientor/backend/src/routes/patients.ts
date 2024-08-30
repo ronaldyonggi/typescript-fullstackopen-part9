5e155952b92de96ab5e7ac054bfadfdc6f30578d
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-30 09:27:30 +0800

    9.13 validate req with type validation

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import patientService from '../services/patientService';
+import toNewPatient from '../utils';
 
 const router = express.Router();
 
@@ -8,8 +9,17 @@ router.get('/', (_req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const addedPatient = patientService.addPatient(req.body);
-  return res.json(addedPatient);
+  try {
+    const newPatient = toNewPatient(req.body);
+    const addedPatient = patientService.addPatient(newPatient);
+    return res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    return res.status(400).send({ error: errorMessage });
+  }
 });
 
 export default router;
