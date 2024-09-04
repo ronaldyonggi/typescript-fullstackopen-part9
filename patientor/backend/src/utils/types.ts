0b9d2617be0ee52ecf297d4532debda551682317
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-04 19:45:07 +0800

    defined BaseEntrySchema

--- patientor/backend/src/utils/types.ts
+++ patientor/backend/src/utils/types.ts
@@ -14,6 +14,14 @@ const DiagnosisSchema = z.object({
 
 export type Diagnosis = z.infer<typeof DiagnosisSchema>;
 
+const BaseEntrySchema = z.object({
+  id: z.string(),
+  description: z.string(),
+  date: z.string().datetime(),
+  specialist: z.string(),
+  diagnosisCodes: z.array(DiagnosisSchema.shape.code).optional(),
+});
+
 export type NewPatient = z.infer<typeof NewPatientSchema>;
 export interface Patient extends NewPatient {
   id: string;
